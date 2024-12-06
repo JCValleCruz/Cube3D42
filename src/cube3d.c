1c7d156e4baecc6979a9b4c9ad9796c4c3ef93fd
Baldizzone42 <baldizzone42@gmail.com>
2024-12-06 19:38:33 +0100

    giros A/D actualizados y actualizamos alpha/v_dir

--- src/cube3d.c
+++ src/cube3d.c
@@ -6,7 +6,7 @@
 /*   By: jormoral <jormoral@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/16 16:15:59 by gacel             #+#    #+#             */
-/*   Updated: 2024/12/06 16:59:17 by jormoral         ###   ########.fr       */
+/*   Updated: 2024/12/06 19:37:47 by jormoral         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -79,13 +79,39 @@ void raycasting(t_cube *file)
 {
 	(void)file;
 }
-
+void lateral_move(t_cube *file, double newmove)
+{
+	file->alpha += newmove;
+	if (file->alpha < 0.0)
+		file->alpha += 2 * PI;
+	if (file->alpha > 2 * PI)
+		file->alpha -= 2 * PI;
+	file->v_dir.x = cos(file->alpha);
+	file->v_dir.y = sin(file->alpha);
+	/* sumamos el nuevo movimiento a nuestro alpha
+	nunca puede estar alpha fuera de el rango de 360\
+	y actualizamos el vector de direcion nuevo;*/
+		
+}
 
 void handle_move(void *param)
 {
 	t_cube *file;
 	file = (t_cube *)param;
 	raycasting(file);
+	/* en base al movimiento que realicemos, tenemos que calcula la direccion
+	si nos movemos hacias los lados, y si nos movemos hacia delante o atras 
+	realizamos el movimiento, esto tiene que estar activo durante
+	todo el programa*/
+	if(mlx_is_key_down(file->mlx, MLX_KEY_ESCAPE))
+		mlx_close_window(file->mlx);
+	else if (mlx_is_key_down(file->mlx, MLX_KEY_A))
+		lateral_move(file, -1 * 0.1745);
+	else if (mlx_is_key_down(file->mlx, MLX_KEY_D))
+		lateral_move(file, 0.1745);
+	/*nuestro giros van a ser de 10 grados, lo que es igual 
+	a PI / 18 = 0.1745, si giramos hacia la izquierda es -1
+	porque estamos girando sobre el eje x */
 }
 
 
@@ -101,16 +127,16 @@ int main(int argc, char **argv)
 	mlx_image_to_window(file.mlx, file.img, 100, 0);
 	mlx_loop_hook(file.mlx, handle_move, &file);
 	mlx_loop(file.mlx);
-	
+	// funcion que libere aqui!!
 	//print_split2(file.map);
 	//printf("\n\n\n\n\n");
 	//print_split2(file.clone_map); 
-/* 	printf("%c\n", file.orientation);
+	/* printf("%c\n", file.orientation);
 	printf("%f\n", file.position_player.x);
 	printf("%f\n", file.position_player.y);
 	printf("%f\n", file.alpha);
 	printf("%f\n", file.v_dir.x);
-	printf("%f\n", file.v_dir.y); */
-
+	printf("%f\n", file.v_dir.y);
+ */
 	return 0;
 }
